eb7a39a8374bcfda79553ca8f5b26b58507569b3
Kartik Kwatra <kartikkwatra2016@gmail.com>
2021-07-02 15:28:53 +0530

    Delete UserActionProvider.ts

--- apps/pagetour-sdk/src/models/UserActionProvider.ts
+++
@@ -1,6 +0,0 @@
-import { UserActions } from './useractions'
-interface UserActionProvider {
-  recordUserAction: (tutorial: any, userAction: string, step: string, operation: string) => Promise<UserActions>
-}
-
-export { UserActionProvider }
